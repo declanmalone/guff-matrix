10a4d38ef8d91ac59001561c531a80ef097ea55f
Declan Malone <idablack@users.sourceforge.net>
2021-07-12 22:12:16 +0100

    Fix up conditional/feature-based compilation. Makes options additive.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,88 @@
+//! Fast SIMD matrix multiplication for finite fields
+//!
+//! This crate implements two things:
+//!
+//! 1. Fast SIMD-based multiplication of vectors of finite field
+//!    elements (GF(2<sup>8</sup>) with the polynomial 0x11b)
+//!
+//! 2. A (cache-friendly) matrix multiplication routine based on
+//!    achieving 100% utilisation of the above
+//!
+//! This crate supports x86_64 and Arm (v7, v8) with NEON extensions.
+//!
+//! The matrix multiplication routine is heavily geared towards use in
+//! implementing Reed-Solomon or Information Dispersal Algorithm
+//! error-correcting codes.
+//!
+//! For x86_64 and Armv8 (Aarch64), building requires no extra
+//! options:
+//!
+//! ```bash
+//! cargo build
+//! ```
+//!
+//! It seems that on armv7 platforms, the rust build system is unable
+//! to detect the availability of `target_feature = ""`. As a
+//! result, I've added "neon" as a build feature instead. Select it
+//! with:
+//!
+//! ```bash
+//! RUSTFLAGS="-C target-cpu=native" cargo build --features neon
+//! ```
+//!
+//! # Software Simulation Feature
+//!
+//! I've implemented a pure Rust version of the matrix multiplication
+//! code. It uses the same basic idea as the optimised versions,
+//! although for clarity, it works a byte at a time instead of
+//! simulating SIMD multiplication on 8 or 16 bytes at a time.
+//!
+//! 
+//!
+//!
 //!
 
 
 #![feature(stdsimd)]
-#[cfg(not(feature = "fake-simd"))]
-#[cfg_attr(any(target_arch = "x86", target_arch = "x86_64"), path = "x86.rs")]
-#[cfg_attr(all(any(target_arch = "aarch64", target_arch = "arm"), target_feature = "neon"), path = "armv7.rs")]
-#[cfg_attr(all(target_arch = "arm"), path = "armv6.rs")]
-//#[cfg_attr(all(, path = "armv8.rs")]
-mod arch;
 
-#[cfg(feature = "fake-simd")]
-mod arch;
+
+// Rationalise target arch/target feature/build feature
+//
+// I have three different arm-based sets of SIMD code:
+//
+// 1. thumb/dsp-based 4-way simd that works on armv6 and armv7, but
+//    not, apparently, on armv8
+//
+// 2. neon-based 16-way reimplementation of the above, which works on
+//    armv7 with neon extension, and armv8
+//
+// 3. new neon-based 8-way simd based on vmull and vtbl instructions,
+//    which works on armv7 with neon extension, and armv8
+//
+// Since I'm controlling compilation by named features, I want all of
+// these to be additive. As a result, I'll give each of them a
+// separate module name, which will appear if the appropriate feature
+// is enabled.
+//
+
+// Only one x86 implementation, included automatically
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+mod x86;
+
+// Implementation (1) above
+#[cfg_attr(all(target_arch = "arm"), feature = "arm_dsp")]
+mod arm_dsp;
+
+// Implementation (2) above
+#[cfg(all(any(target_arch = "aarch64", target_arch = "arm"), feature = "arm_long"))]
+mod arm_long;
+
+// Implementation (3) above
+#[cfg(all(any(target_arch = "aarch64", target_arch = "arm"), feature = "arm_vmull"))]
+mod arm_vmull;
+
+#[cfg(feature = "simulator")]
+mod simulator;
 
 pub fn gcd(mut a : usize, mut b : usize) -> usize {
   let mut t;
@@ -62,7 +134,7 @@ struct TransformTape {
 // The only fly in the ointment is if I need fundamentally different
 // logic to map operations onto intrinsics...
 
-#[macro_export]
+// #[macro_export]
 macro_rules! new_xform_reader {
     ( $s:ident, $k:expr, $n:expr, $w:expr, $r0:ident, $r1:ident) => {
 	let mut $s = TransformTape { k : $k, n : $n, w : $w, xptr : 0 };
